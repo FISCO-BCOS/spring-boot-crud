8655f8966afe674b58df102cc0e0ba00b50a209a
kyonRay <rui.kwok@outlook.com>
2021-11-22 09:50:43 +0800

    <project>(build): fix build error.

--- src/main/java/com/fisco/app/client/CRUDMultiParameterClient.java
+++ src/main/java/com/fisco/app/client/CRUDMultiParameterClient.java
@@ -1,7 +1,7 @@
 package com.fisco.app.client;
 
 import com.fisco.app.common.CommonClientMultiParameter;
-import com.fisco.app.contract.TestMultiParameterCRUD;
+import com.fisco.app.contract.TestCRUDMultiParameter;
 import com.fisco.app.utils.SpringUtils;
 import org.fisco.bcos.sdk.BcosSDK;
 import org.fisco.bcos.sdk.model.TransactionReceipt;
@@ -23,8 +23,8 @@ import java.util.List;
 public class CRUDMultiParameterClient extends CommonClientMultiParameter implements ApplicationRunner {
     @Override
     public boolean insert(List<String> params) {
-        TestMultiParameterCRUD testMultiParameterCRUD = (TestMultiParameterCRUD) getContractMap().get("TestMultiParameterCRUD");
-        TransactionReceipt receipt = testMultiParameterCRUD.insert(params);
+        TestCRUDMultiParameter testCRUDMultiParameter = (TestCRUDMultiParameter) getContractMap().get("TestCRUDMultiParameter");
+        TransactionReceipt receipt = testCRUDMultiParameter.insert(params);
         logger.info("调用CRUDClientMultiParameter的insert方法");
         logger.info("结果：{}", receipt);
         return true;
@@ -32,8 +32,8 @@ public class CRUDMultiParameterClient extends CommonClientMultiParameter impleme
 
     @Override
     public List query(String name) throws ContractException {
-        TestMultiParameterCRUD testMultiParameterCRUD = (TestMultiParameterCRUD) getContractMap().get("TestMultiParameterCRUD");
-        List result = Collections.singletonList(testMultiParameterCRUD.select(name));
+        TestCRUDMultiParameter testCRUDMultiParameter = (TestCRUDMultiParameter) getContractMap().get("TestCRUDMultiParameter");
+        List result = Collections.singletonList(testCRUDMultiParameter.select(name));
         logger.info("调用CRUDClientMultiParameter的query方法");
         logger.info("结果：{}", result);
         return result;
@@ -41,8 +41,8 @@ public class CRUDMultiParameterClient extends CommonClientMultiParameter impleme
 
     @Override
     public boolean edit(List<String> params) {
-        TestMultiParameterCRUD testMultiParameterCRUD = (TestMultiParameterCRUD) getContractMap().get("TestMultiParameterCRUD");
-        TransactionReceipt receipt = testMultiParameterCRUD.update(params);
+        TestCRUDMultiParameter testCRUDMultiParameter = (TestCRUDMultiParameter) getContractMap().get("TestCRUDMultiParameter");
+        TransactionReceipt receipt = testCRUDMultiParameter.update(params);
         logger.info("调用CRUDClientMultiParameter的edit方法");
         logger.info("结果：{}",receipt);
         return true;
@@ -50,8 +50,8 @@ public class CRUDMultiParameterClient extends CommonClientMultiParameter impleme
 
     @Override
     public boolean remove(String name) {
-        TestMultiParameterCRUD testMultiParameterCRUD = (TestMultiParameterCRUD) getContractMap().get("TestMultiParameterCRUD");
-        TransactionReceipt receipt = testMultiParameterCRUD.remove(name);
+        TestCRUDMultiParameter testCRUDMultiParameter = (TestCRUDMultiParameter) getContractMap().get("TestCRUDMultiParameter");
+        TransactionReceipt receipt = testCRUDMultiParameter.remove(name);
         logger.info("调用CRUDClientMultiParameter的remove方法");
         logger.info("结果：{}",receipt);
         return true;
@@ -60,6 +60,6 @@ public class CRUDMultiParameterClient extends CommonClientMultiParameter impleme
     @Override
     public void run(ApplicationArguments args) throws Exception {
         BcosSDK sdk = SpringUtils.getBean("bcosSDK");
-        deploy("TestMultiParameterCRUD", TestMultiParameterCRUD.class, sdk, "group");
+        deploy("TestCRUDMultiParameter", TestCRUDMultiParameter.class, sdk, "group");
     }
 }
--- src/main/resources/contract/TestCRUDMultiParameter.sol
+++ src/main/resources/contract/TestCRUDMultiParameter.sol
@@ -5,97 +5,106 @@ import "./Table.sol";
 
 
 contract TestCRUDMultiParameter {
-    
-    event CreateResult(int256 count);
+
     event InsertResult(int256 count);
     event UpdateResult(int256 count);
     event RemoveResult(int256 count);
-    
-    TableFactory tableFactory;
+
+    Table table;
     string constant TABLE_NAME = "person";
     //定义属性，多参数通过数组解决,数组第一个值为主键
     string []  properties = ["name","age","tel"];
     constructor() public {
-        tableFactory = TableFactory(0x1001); 
-        tableFactory.createTable(TABLE_NAME, "name", "age,tel");
+        table = Table(0x1001); //The fixed address is 0x1001 for TableFactory
+        // the parameters of createTable are tableName,keyField,"vlaueFiled1,vlaueFiled2,vlaueFiled3,..."
+        table.createTable(TABLE_NAME, "name", "age,tel");
     }
 
-     //插入数据
-    function insert(string [] entity)
+    //插入数据
+    function insert(string[] memory entity)
     public
     returns (int256)
     {
-        Table table = tableFactory.openTable(TABLE_NAME);
-        
-        Entry entry = table.newEntry();
+        require(entity.length == 3, "entity length mismatch");
+        KVField[] memory KVFields = new KVField[](3);
         for (uint256 i = 0; i < properties.length; ++i) {
-            entry.set(properties[i], entity[i]);
+            KVField memory kv = KVField(properties[i], entity[i]);
+            KVFields[i] = kv;
         }
+        Entry memory entry = Entry(KVFields);
 
-        int256 count = table.insert(entry);
+        int256 count = table.insert(TABLE_NAME,entry);
         emit InsertResult(count);
 
         return count;
     }
-    
-     //查询数据
+
+    //查询数据
     function select(string memory name)
     public
     view
     returns (string[] memory,string[] memory,string[] memory)
     {
-        Table table = tableFactory.openTable(TABLE_NAME);
+        CompareTriple memory compareTriple1 = CompareTriple("name",name,Comparator.EQ);
+        CompareTriple[] memory compareFields = new CompareTriple[](1);
+        compareFields[0] = compareTriple1;
 
-        Condition condition = table.newCondition();
-        condition.EQ("name", name);
-        Entries entries = table.select(condition);
-        string[] memory user_name_bytes_list = new string[](uint256(entries.size()));
-        string[] memory age_list = new string[](uint256(entries.size()));
-        string[] memory tel_list = new string[](uint256(entries.size()));
-     
-        for (int256 i = 0; i <entries.size(); ++i) {
-            Entry entry = entries.get(i);
-            user_name_bytes_list[uint256(i)] = entry.getString(properties[0]);
-            age_list[uint256(i)] = entry.getString(properties[1]);
-            tel_list[uint256(i)] = entry.getString(properties[2]);
+        Condition memory condition;
+        condition.condFields = compareFields;
+
+        Entry[] memory entries = table.select(TABLE_NAME, condition);
+        string[] memory user_name_bytes_list = new string[](uint256(entries.length));
+        string[] memory age_list = new string[](uint256(entries.length));
+        string[] memory tel_list = new string[](uint256(entries.length));
+        if(entries.length > 0){
+            for (uint256 i = 0; i < entries.length; ++i) {
+                user_name_bytes_list[uint256(i)] = name;
+                age_list[uint256(i)] = entries[i].fields[0].value;
+                tel_list[uint256(i)] = entries[i].fields[1].value;
+            }
         }
-
-        return (user_name_bytes_list,age_list,tel_list);
+        return (user_name_bytes_list, age_list, tel_list);
     }
-    
-     //修改数据
-    function update(string [] entity)
+
+    //修改数据
+    function update(string[] memory entity)
     public
     returns (int256)
     {
-        Table table = tableFactory.openTable(TABLE_NAME);
+        require(entity.length == 3, "entity length mismatch");
+        KVField[] memory KVFields = new KVField[](2);
 
-        Entry entry = table.newEntry();
         for (uint256 i = 1; i < entity.length; ++i) {
-            entry.set(properties[i], entity[i]);
+            KVField memory kv = KVField(properties[i], entity[i]);
+            KVFields[i-1] = kv;
         }
+        Entry memory entry = Entry(KVFields);
 
-        Condition condition = table.newCondition();
-        condition.EQ(properties[0], entity[0]);
+        CompareTriple memory compareTriple1 = CompareTriple(properties[0], entity[0],Comparator.EQ);
+        CompareTriple[] memory compareFields = new CompareTriple[](1);
+        compareFields[0] = compareTriple1;
 
-        int256 count = table.update(entry, condition);
+        Condition memory condition;
+        condition.condFields = compareFields;
+
+        int256 count = table.update(TABLE_NAME,entry, condition);
         emit UpdateResult(count);
 
         return count;
     }
-    
-     //删除数据
+
+    //删除数据
     function remove(string memory name) public returns (int256) {
-        Table table = tableFactory.openTable(TABLE_NAME);
+        CompareTriple memory compareTriple1 = CompareTriple("name",name,Comparator.EQ);
+        CompareTriple[] memory compareFields = new CompareTriple[](1);
+        compareFields[0] = compareTriple1;
 
-        Condition condition = table.newCondition();
-        condition.EQ(properties[0], name);
-
-        int256 count = table.remove(condition);
-        emit RemoveResult(count);
-
-        return count;
+        Condition memory condition;
+        condition.condFields = compareFields;
+        int256 result = table.remove(TABLE_NAME, condition);
+        emit RemoveResult(result);
+        return result;
     }
-    
-   
+
+
 }
